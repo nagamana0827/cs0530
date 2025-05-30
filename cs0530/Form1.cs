a23b64886e4ebc31dc67ea74d25bc67fef84ee7c
nagayamamana <nagamiyama1031@gmail.com>
2025-05-30 14:23:55 +0900

    a

--- cs0530/Form1.cs
+++ cs0530/Form1.cs
@@ -2,9 +2,17 @@ namespace cs0530
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
